59a6acb0fba938cc4e877f86979771bc1843f7fc
Ievgen-Rybalko <jackrjack1@gmail.com>
2021-10-15 00:13:04 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const { listContacts,
+    getContactById,
+    removeContact,
+    addContact } = require("./contacts");
+
+
+// index.js
+const argv = require('yargs').argv;
+
+// TODO: рефакторить
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+      case 'list':
+        const contacts = await listContacts();
+        console.table(contacts);
+        break;
+  
+      case 'get':
+        const contact = await getContactById(id);
+        if (!contact){
+            throw new Error(`Contact: id=${id} was not found`);
+        }
+        console.log(contact);
+        break;
+  
+      case 'add':
+        await addContact(name, email, phone);
+        console.log("Successfully added");
+        break;
+  
+      case 'remove':
+        const result = await removeContact(id);
+        if (!result){
+            throw new Error(`Contact: id=${id} was not found`);
+        }
+        console.log("Successfully removed");
+        break;
+      
+      default:
+        console.warn('\x1B[31m Unknown action type!');
+    }
+  }
+  
+  invokeAction(argv);
